2efd9d0d89f0f3080789315e578178acfd8c8e3b
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-02-24 09:18:42 -0300

    FEAT: Inicialização do Projeto com Movimentação Básica de Personagem e Câmera seguindo o Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rigidBody2D;
+    [SerializeField] private TrailRenderer trailRenderer;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 15f;
+    private Vector2 moveDirection;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpPower = 10f;
+    [SerializeField] private bool isJumping = false;
+
+    [Header("Gravity Settings")]
+    [SerializeField] private float gravityScale = 5f;
+    [SerializeField] private float fallGravityScale = 15f;
+
+    private void Start() {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.gravityScale = gravityScale;
+
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    private void Update() {
+
+    }
+
+    private void FixedUpdate() {
+        rigidBody2D.linearVelocity = new Vector2(moveDirection.x * moveSpeed, rigidBody2D.linearVelocity.y);
+        AdjustGravity();
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (context.started && !isJumping) {
+            rigidBody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+        if (context.canceled && rigidBody2D.linearVelocity.y > 0) {
+            rigidBody2D.linearVelocity = new Vector2(rigidBody2D.linearVelocity.x, rigidBody2D.linearVelocity.y * 0.6f);
+        }
+    }
+
+    private void AdjustGravity() {
+        if (rigidBody2D.linearVelocity.y > 0) {
+            rigidBody2D.gravityScale = gravityScale;
+        } else {
+            rigidBody2D.gravityScale = fallGravityScale;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        moveDirection = context.ReadValue<Vector2>();
+        trailRenderer.emitting = moveDirection.x != 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isJumping = false;
+        }
+    }
+}
+
