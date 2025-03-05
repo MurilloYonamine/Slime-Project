fdf84144df3d06310fb3de93fbef56dd246f3b0b
anotter dev <148708467+anotter-random-dev@users.noreply.github.com>
2025-03-05 01:07:06 -0300

    feat: adicionei o sistema de massa

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,14 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace PLAYER
 {
+    
     public class PlayerShoot : MonoBehaviour
     {
+        public PlayerHealth pHealth;
+        
         [SerializeField] private GameObject bulletPrefab;
         [SerializeField] private float bulletSpeed = 50f;
         [SerializeField] private Camera mainCamera;
         [SerializeField] private RectTransform aimPrefab;
+
 
         private void Update()
         {
@@ -17,11 +22,14 @@ namespace PLAYER
 
         public void Shoot(InputAction.CallbackContext context)
         {
-            if (context.started)
+            
+            if (context.started && pHealth.Health > 1)
             {
+                pHealth.Health -= 1;
                 Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
                 Vector3 shootDirection = (mousePosition - transform.position).normalized;
+ 
 
                 GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
                 bullet.GetComponent<Rigidbody2D>().linearVelocity = new Vector3(shootDirection.x, shootDirection.y, shootDirection.z) * bulletSpeed;
