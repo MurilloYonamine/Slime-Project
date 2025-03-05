c13af252b5c279bb275adf4e4c775e196b2257c5
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-03-05 16:57:00 -0300

    FIX: Faltou a chamada do namespace do Player.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
-public class PlayerHealth : MonoBehaviour
-{
-    public float Health;
-    public float MaxHealth;
+namespace PLAYER {
+    public class PlayerHealth : MonoBehaviour {
+        public float Health;
+        public float MaxHealth;
 
-    public Image healthBar;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        MaxHealth = Health;
-    }
+        public Image healthBar;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start() {
+            MaxHealth = Health;
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        healthBar.fillAmount = Mathf.Clamp(Health / MaxHealth, 0, 1);
+        // Update is called once per frame
+        void Update() {
+            healthBar.fillAmount = Mathf.Clamp(Health / MaxHealth, 0, 1);
+        }
     }
 }
