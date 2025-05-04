5d2db2f8bb38f689b0c53f5ee46a102c0b1506af
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-04 13:50:04 -0300

    FEAT: Agora o script dá um feedback visual quando o jogador está perto da range do grappler.

--- Assets/Scripts/Player/PlayerGrappler.cs
+++ Assets/Scripts/Player/PlayerGrappler.cs
@@ -16,27 +16,30 @@ namespace PLAYER
         private Rigidbody2D rigidBody2D;
 
         private LayerMask grapplerLayer;
+        private LayerMask grapplerArea;
         [SerializeField] private float grappleMaxPoint = 5f;
         [HideInInspector] public bool IsGrappleWithinMaxDistance { get; private set; } = true;
 
-        public void Initialize(PlayerController player, LineRenderer lineRenderer, DistanceJoint2D distanceJoint2D, LayerMask grapplerLayer, Rigidbody2D rigidBody2D)
+        private Color grapplerObjectOriginalColor;
+
+        public void Initialize(PlayerController player, LineRenderer lineRenderer, DistanceJoint2D distanceJoint2D, LayerMask grapplerLayer, LayerMask grapplerArea, Rigidbody2D rigidBody2D)
         {
             this.player = player;
             this.lineRenderer = lineRenderer;
             this.distanceJoint2D = distanceJoint2D;
             this.grapplerLayer = grapplerLayer;
+            this.grapplerArea = grapplerArea;
             this.rigidBody2D = rigidBody2D;
-
-            this.IsGrappleWithinMaxDistance = true;
 
             lineRenderer.enabled = false;
             distanceJoint2D.enabled = false;
+            
+            GameManager.Instance.ChangeGrapplersDistance(grappleMaxPoint);
         }
 
         public void OnUpdate()
         {
             if (lineRenderer.enabled) lineRenderer.SetPosition(1, player.transform.position);
-            
         }
 
         public void Grapple(InputAction.CallbackContext context)
@@ -47,12 +50,13 @@ namespace PLAYER
 
                 RaycastHit2D hit = Physics2D.Raycast(player.transform.position, mousePosition - (Vector2)player.transform.position, Mathf.Infinity, grapplerLayer);
 
+                float distanceToGrapplePoint = Vector2.Distance(player.transform.position, hit.point);
+
                 if (hit.collider != null)
                 {
-                    float distanceToGrapplePoint = Vector2.Distance(player.transform.position, hit.point);
                     if (distanceToGrapplePoint <= grappleMaxPoint)
                     {
-                        player.CanGrapple = true;
+                        player.IsGrappling = true;
                         lineRenderer.SetPosition(0, hit.point);
                         lineRenderer.SetPosition(1, player.transform.position);
 
@@ -60,13 +64,6 @@ namespace PLAYER
                         distanceJoint2D.enabled = true;
                         lineRenderer.enabled = true;
                         AudioManager.Instance.PlaySoundEffect("Audio/SFX/Slime/slime_shot", volume: 1f);
-
-                        // Adiciona rotação ao jogador
-                        rigidBody2D.angularVelocity = 360f; // Velocidade de rotação em graus por segundo
-                    }
-                    else
-                    {
-                        player.CanGrapple = false;
                     }
                 }
             }
@@ -74,14 +71,30 @@ namespace PLAYER
             {
                 distanceJoint2D.enabled = false;
                 lineRenderer.enabled = false;
-                player.CanGrapple = false;
-
-                // Para a rotação quando o grappler é cancelado
-                rigidBody2D.angularVelocity = 0f;
+                player.IsGrappling = false;
             }
 
             if (lineRenderer.enabled) lineRenderer.SetPosition(1, player.transform.position);
         }
+        public void TriggerEnter2D(Collider2D collider2D)
+        {
+            if (((1 << collider2D.gameObject.layer) & grapplerArea) != 0)
+            {
+                player.CanGrapple = true;
 
+                grapplerObjectOriginalColor = collider2D.gameObject.GetComponentInParent<SpriteRenderer>().color;
+                collider2D.gameObject.GetComponentInParent<SpriteRenderer>().color = Color.yellow;
+                return;
+            }
+        }
+        public void TriggerExit2D(Collider2D collider2D)
+        {
+            if (((1 << collider2D.gameObject.layer) & grapplerArea) != 0)
+            {
+                player.CanGrapple = false;
+
+                collider2D.gameObject.GetComponentInParent<SpriteRenderer>().color = grapplerObjectOriginalColor;
+            }
+        }
     }
 }
