ec49f710d7a7c90ef208cad93e0c4ac0e5818194
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-04 13:50:20 -0300

    REFACTOR: Apenas dei uma organizada no código.

--- Assets/Scripts/Player/PlayerSpike.cs
+++ Assets/Scripts/Player/PlayerSpike.cs
@@ -2,9 +2,11 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace PLAYER {
+namespace PLAYER
+{
     [Serializable]
-    public class PlayerSpike {
+    public class PlayerSpike
+    {
         [HideInInspector] public PlayerController player;
         [HideInInspector] public bool CanScale = true;
 
@@ -13,51 +15,64 @@ namespace PLAYER {
         [SerializeField] private float yScale = 0.5f;
         [SerializeField] private float smoothTime = 0.2f;
         [SerializeField] private Vector2 targetScale;
-        private Vector2 velocity = new Vector2(2,2);
+        private Vector2 velocity = new Vector2(2, 2);
         private Vector2 initialScale;
         private float CurTime;
 
-        public void Initialize(PlayerController player) {
+        public void Initialize(PlayerController player)
+        {
             this.player = player;
             this.initialScale = player.transform.localScale;
             this.targetScale = initialScale;
-            CurTime = smoothTime+0.8f;
+            CurTime = smoothTime + 0.8f;
         }
-        public void OnUpdate() {
-            if (player.cursize == PlayerController.CURSIZE.normal){
-                this.initialScale = new Vector2(1f,1f);
+        public void OnUpdate()
+        {
+            if (player.cursize == PlayerController.CURSIZE.normal)
+            {
+                this.initialScale = new Vector2(1f, 1f);
                 xScale = 4f;
                 yScale = 0.5f;
-            } else{
-                this.initialScale = new Vector2(0.5f,0.5f);
+            }
+            else
+            {
+                this.initialScale = new Vector2(0.5f, 0.5f);
                 xScale = 2f;
                 yScale = 0.25f;
             }
 
 
-            if (CanScale){
+            if (CanScale)
+            {
                 CurTime -= Time.deltaTime;
                 player.transform.localScale = Vector2.SmoothDamp(player.transform.localScale, targetScale, ref velocity, smoothTime);
-                if (CurTime <= 0){
+                if (CurTime <= 0)
+                {
                     CanScale = false;
-                    CurTime = smoothTime+0.8f;
-                    if (player.curstretch == PlayerController.CURSTRECH.normal){
+                    CurTime = smoothTime + 0.8f;
+                    if (player.curstretch == PlayerController.CURSTRECH.normal)
+                    {
                         player.curstretch = PlayerController.CURSTRECH.steched;
-                    } else if (player.curstretch == PlayerController.CURSTRECH.steched){
+                    }
+                    else if (player.curstretch == PlayerController.CURSTRECH.steched)
+                    {
                         player.curstretch = PlayerController.CURSTRECH.normal;
                     }
                 }
             }
         }
-        public void Spike(InputAction.CallbackContext context) {
-            if (context.performed && !CanScale) {
+        public void Spike(InputAction.CallbackContext context)
+        {
+            if (context.performed && !CanScale)
+            {
                 player.IsSpikeActive = !player.IsSpikeActive;
                 targetScale = new Vector2(player.IsSpikeActive ? xScale : initialScale.x, player.IsSpikeActive ? yScale : initialScale.y);
                 AllowedScalingFor90PercentOff();
             }
 
         }
-        public void DisableSpike() {
+        public void DisableSpike()
+        {
             player.IsSpikeActive = false;
             targetScale = initialScale;
         }
