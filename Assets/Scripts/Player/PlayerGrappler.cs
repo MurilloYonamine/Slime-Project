a5578f377bd6807ad39104cad2c54c602b0427d6
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-04 12:01:28 -0300

    CHORE: removi algunas parametros para tornar o código mais limpo no PlayerGrappler.

--- Assets/Scripts/Player/PlayerGrappler.cs
+++ Assets/Scripts/Player/PlayerGrappler.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-namespace PLAYER {
+namespace PLAYER
+{
     [Serializable]
-    public class PlayerGrappler {
+    public class PlayerGrappler
+    {
         private PlayerController player;
 
         private LineRenderer lineRenderer;
         private DistanceJoint2D distanceJoint2D;
+        private Rigidbody2D rigidBody2D;
 
         private LayerMask grapplerLayer;
         [SerializeField] private float grappleMaxPoint = 5f;
         [HideInInspector] public bool IsGrappleWithinMaxDistance { get; private set; } = true;
 
-        public void Initialize(PlayerController player, LineRenderer lineRenderer, DistanceJoint2D distanceJoint2D, LayerMask grapplerLayer) {
+        public void Initialize(PlayerController player, LineRenderer lineRenderer, DistanceJoint2D distanceJoint2D, LayerMask grapplerLayer, Rigidbody2D rigidBody2D)
+        {
             this.player = player;
             this.lineRenderer = lineRenderer;
             this.distanceJoint2D = distanceJoint2D;
             this.grapplerLayer = grapplerLayer;
+            this.rigidBody2D = rigidBody2D;
 
             this.IsGrappleWithinMaxDistance = true;
 
@@ -28,19 +33,25 @@ namespace PLAYER {
             distanceJoint2D.enabled = false;
         }
 
-        public void OnUpdate() {
+        public void OnUpdate()
+        {
             if (lineRenderer.enabled) lineRenderer.SetPosition(1, player.transform.position);
+            
         }
 
-        public void Grapple(InputAction.CallbackContext context) {
-            if (context.started) {
+        public void Grapple(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
                 Vector2 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
 
                 RaycastHit2D hit = Physics2D.Raycast(player.transform.position, mousePosition - (Vector2)player.transform.position, Mathf.Infinity, grapplerLayer);
 
-                if (hit.collider != null) {
+                if (hit.collider != null)
+                {
                     float distanceToGrapplePoint = Vector2.Distance(player.transform.position, hit.point);
-                    if (distanceToGrapplePoint <= grappleMaxPoint) {
+                    if (distanceToGrapplePoint <= grappleMaxPoint)
+                    {
                         player.CanGrapple = true;
                         lineRenderer.SetPosition(0, hit.point);
                         lineRenderer.SetPosition(1, player.transform.position);
@@ -49,17 +60,28 @@ namespace PLAYER {
                         distanceJoint2D.enabled = true;
                         lineRenderer.enabled = true;
                         AudioManager.Instance.PlaySoundEffect("Audio/SFX/Slime/slime_shot", volume: 1f);
-                    } else {
+
+                        // Adiciona rotação ao jogador
+                        rigidBody2D.angularVelocity = 360f; // Velocidade de rotação em graus por segundo
+                    }
+                    else
+                    {
                         player.CanGrapple = false;
                     }
                 }
-            } else if (context.canceled) {
+            }
+            else if (context.canceled)
+            {
                 distanceJoint2D.enabled = false;
                 lineRenderer.enabled = false;
                 player.CanGrapple = false;
+
+                // Para a rotação quando o grappler é cancelado
+                rigidBody2D.angularVelocity = 0f;
             }
 
             if (lineRenderer.enabled) lineRenderer.SetPosition(1, player.transform.position);
         }
+
     }
 }
