ed01372aef1db9a73974406c0ffb37b23a225f11
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-06 20:44:36 -0300

    FEAT: Transformei o script Moving Platform em um script pai para utilizarmos herança no projeto.

---
+++ Assets/Scripts/Plataforms/Platform.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace PLATFORMS {
+    public class Platform : MonoBehaviour {
+
+        [SerializeField] private Transform pointA;
+        [SerializeField] private Transform pointB;
+        [SerializeField] private float moveSpeed = 2f;
+
+        [SerializeField] private Vector3 NextPosition;
+
+        protected virtual void Start() {
+            NextPosition = pointA.position;
+        }
+
+        protected virtual void Update() {
+            transform.position = Vector2.MoveTowards(transform.position, NextPosition, moveSpeed * Time.deltaTime);
+
+            if (transform.position == NextPosition)
+                NextPosition = (NextPosition == pointA.position) ? pointB.position : pointA.position;
+        }
+        protected virtual void OnCollisionEnter2D(Collision2D collision) {
+            if (collision.gameObject.CompareTag("Player"))
+                collision.gameObject.transform.parent = transform;
+        }
+        protected virtual void OnCollisionExit2D(Collision2D collision) {
+            if (collision.gameObject.CompareTag("Player"))
+                collision.gameObject.transform.parent = GameManager.Instance.PlayerOriginalLayer;
+        }
+
+    }
+}
