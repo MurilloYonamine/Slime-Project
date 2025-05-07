6242267e97134898fa6a7d33b894c92ddb3b6d2a
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-07 10:44:56 -0300

    FIX: Consertei uns erros na plataforma que desaparece.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public GameObject player;
 
-    [SerializeField] private TextMeshProUGUI magicSeedCountTxt;
-    [HideInInspector] public int magicSeedCount = 0;
 
     [SerializeField] public Transform PlayerOriginalLayer;
 
@@ -29,7 +27,6 @@ public class GameManager : MonoBehaviour
         }
         //AudioManager.Instance.PlayTrack("Audio/Music/test-song", loop: true);
     }
-    private void Update() => magicSeedCountTxt.text = $"Magic Seed Count: {magicSeedCount}";
     public void Die() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     public void ChangeGrapplersDistance(float distance) {
         foreach(GameObject grappler in allGrapplers) {
