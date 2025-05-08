d8d0d4170614b768b4db34158b4bd11b707bad29
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-08 20:15:57 -0300

    FIX: Consertei o Bug na câmera caso o jogador fique indo pra lá e pra cá no colisor.

---
+++ Assets/Scripts/Camera/CameraZone.cs
@@ -0,0 +1,37 @@
+using Cinemachine;
+using UnityEngine;
+
+namespace CAMERA {
+    [RequireComponent(typeof(Collider2D))]
+    public class CameraZone : MonoBehaviour {
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        [SerializeField] private BoxCollider2D boxCollider2D;
+
+        private void Awake() {
+            if (virtualCamera == null) virtualCamera = GetComponent<CinemachineVirtualCamera>();
+            if (boxCollider2D == null) boxCollider2D = GetComponent<BoxCollider2D>();
+
+            boxCollider2D.isTrigger = true;
+            boxCollider2D.size = CalculateCameraBounds();
+        }
+        private Vector2 CalculateCameraBounds() {
+            float orthoSize = virtualCamera.m_Lens.OrthographicSize;
+            float aspect = Camera.main.aspect;
+
+            float height = orthoSize * 2f;
+            float width = height * aspect;
+
+            return new Vector2(width, height);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player"))
+                virtualCamera.Priority = 1;
+        }
+
+        private void OnTriggerExit2D(Collider2D other) {
+            if (other.CompareTag("Player"))
+                virtualCamera.Priority = 0;
+        }
+    }
+}
