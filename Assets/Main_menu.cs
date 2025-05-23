4553f6925acdfb2596d5402e735e1dc7c487a51a
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-22 21:24:31 -0300

    FIX: Consertei o cursor, consertei colisoes na fase 1 e 2 na unity e consertei bugs de transições.

--- Assets/Main_menu.cs
+++ Assets/Main_menu.cs
@@ -48,6 +48,11 @@ public class Main_menu : MonoBehaviour {
      }
 
      public void FUCKYOU() {
-          Application.Quit();
+        StartCoroutine(CloseGame());
      }
+    public IEnumerator CloseGame() {
+        StartCoroutine(LoadSceneTransition());
+        yield return new WaitForSeconds(1.5f);
+        Application.Quit();
+    }
 }
--- Assets/Scripts/Plataforms/ShootToStopPlatform.cs
+++ Assets/Scripts/Plataforms/ShootToStopPlatform.cs
@@ -2,38 +2,36 @@ using PLATFORMS;
 using PLAYER;
 using UnityEditor;
 using UnityEngine;
-namespace PLATFORMS{
-public class ShootToStopPlatform : Platform
-{
+namespace PLATFORMS {
+    public class ShootToStopPlatform : Platform {
 
 
-    [HideInInspector] private bool ison = true;
-    [HideInInspector] private float oldspeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    protected override void Start()
-    {
-        base.Start();
-        oldspeed = moveSpeed;
-    }
+        public bool ison = true;
+        [HideInInspector] public float oldspeed;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        protected override void Start() {
+            base.Start();
+            oldspeed = moveSpeed;
+        }
 
-    // Update is called once per frame
+        // Update is called once per frame
 
 
-    private void OnTriggerEnter2D(Collider2D collider2D) {
+        private void OnTriggerEnter2D(Collider2D collider2D) {
 
-        if (collider2D.TryGetComponent<PlayerBullet>(out PlayerBullet bullet)) {
+            if (collider2D.TryGetComponent<PlayerBullet>(out PlayerBullet bullet)) {
                 CHANGE();
             }
         }
 
-    void CHANGE(){
-        ison = !ison;
-            if (!ison){
+        private void CHANGE() {
+            ison = !ison;
+            if (!ison) {
                 moveSpeed = 0;
-            } else{
+            } else {
                 moveSpeed = oldspeed;
             }
-    }
+        }
 
     }
 }
