7f1c4b8465a67a19c76cd29d8ab0b68463d84136
anotter dev <148708467+anotter-random-dev@users.noreply.github.com>
2025-05-29 21:08:15 -0300

    feat: botão das plataformas de cor muda de cor ao levar um tiro

--- Assets/Scripts/Plataforms/ColorButton.cs
+++ Assets/Scripts/Plataforms/ColorButton.cs
@@ -8,15 +8,34 @@ namespace PLATFORMS {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         [SerializeField] ColorPlatform RedPlatform;
         [SerializeField] ColorPlatform BluePlatform;
+        [SerializeField] private Sprite Bluelook;
+        [SerializeField] private Sprite Redlook;
+        private SpriteRenderer butt;
+        private bool isactive = true;
+
+        private void Start() {
+            butt = GetComponent<SpriteRenderer>();
+        }
         private void OnTriggerEnter2D(Collider2D collider2D) {
 
             if (collider2D.TryGetComponent<PlayerBullet>(out PlayerBullet bullet)) {
                 RedPlatform.CHANGE();
                 BluePlatform.CHANGE();
-    
+                CHANGE();
             }
+
+
         }
 
+        private void CHANGE() {
+            isactive = !isactive;
+            if (!isactive) {
+                butt.sprite = Redlook;
+            }
+            else {
+                butt.sprite = Bluelook;
+            }
+        }
 
 
     }
