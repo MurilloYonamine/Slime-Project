b24b4d8bcecc49712484db8e25d2a88fc31426e0
anotter-random-dev <148708467+anotter-random-dev@users.noreply.github.com>
2025-05-30 09:07:02 -0300

    fix: botões de cor muda a imagem

--- Assets/Scripts/Plataforms/ColorButton.cs
+++ Assets/Scripts/Plataforms/ColorButton.cs
@@ -8,16 +8,34 @@ namespace PLATFORMS {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         [SerializeField] ColorPlatform RedPlatform;
         [SerializeField] ColorPlatform BluePlatform;
+        private bool changed = true;
+        [SerializeField] private Sprite redass;
+        [SerializeField] private Sprite blueass;
+        private SpriteRenderer selfspriteshit;
+
+
+        private void Start() {
+            selfspriteshit = GetComponent<SpriteRenderer>();
+        }
         private void OnTriggerEnter2D(Collider2D collider2D) {
 
             if (collider2D.TryGetComponent<PlayerBullet>(out PlayerBullet bullet)) {
                 RedPlatform.CHANGE();
                 BluePlatform.CHANGE();
-    
+                CHANGE();
             }
         }
 
+        private void CHANGE() {
+            changed = !changed;
+            if (changed) {
+                selfspriteshit.sprite = blueass;
+            }
+            else {
+                selfspriteshit.sprite = redass;
+            }
 
+        }
 
     }
 }
